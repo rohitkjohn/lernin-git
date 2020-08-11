ee50120286524631798fa97cd8855d40dad7bc11
Rohit John <rohitkjohn@gmail.com>
2020-08-11 13:22:26 +0200

    jackson

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 nee po mone dhineshaa!!
 Nadesha kollanda
+as he came into the window
+there was a sound of 
+a cheshendo
+
+As he came into the apartemnet
+there were blood stains on the carpet
+
+as she ran under the table
+he could see she
+was unable
+
+then shee came into the bedroom
+she was struck down
+it was her doom
+
